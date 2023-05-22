d00bcee4579b7452ba335eaa3d5fec71a6fa03e5
anakon73 <nazarkinah73@gmail.com>
2023-05-22 21:39:04 +0300

    chore(eslint): install & init

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    '@antfu',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+}
