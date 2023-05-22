79532b19d2fc34af1188f989aafc575ad6e95af0
anakon73 <nazarkinah73@gmail.com>
2023-05-22 22:56:05 +0300

    chore(eslint): tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,19 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
     '@antfu',
+    'plugin:tailwindcss/recommended',
     './.eslintrc-auto-import.json',
   ],
   overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.vue'],
+      parser: '@typescript-eslint/parser',
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <h1 className="text-3xl font-bold underline">
+  <h1 className="flex text-3xl font-bold underline">
     Hello world!
   </h1>
 </template>
