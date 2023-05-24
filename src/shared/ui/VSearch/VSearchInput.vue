9efe0f2aef9b515b3d10903c0b8beb2cfff6739f
anakon73 <nazarkinah73@gmail.com>
2023-05-24 18:50:25 +0300

    feat(ui): implement search input component

---
+++ src/shared/ui/VSearch/VSearchInput.vue
@@ -0,0 +1,32 @@
+<script setup lang='ts'>
+export type Props = {
+  modelValue?: string
+}
+
+const props = defineProps<Props>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+function updateModelValue(event: Event) {
+  if (event.target instanceof HTMLInputElement) {
+    const { value } = event.target
+
+    emits('update:modelValue', value)
+  }
+}
+</script>
+
+<template>
+  <input
+    class="
+    rounded-xl border border-zinc-100 bg-inherit px-4 py-2.5
+    text-zinc-100 placeholder:text-zinc-100 focus:outline-none
+    "
+    :value="props.modelValue"
+    placeholder="Search..."
+    type="text"
+    @input="updateModelValue"
+  >
+</template>
---
+++ src/shared/ui/VSearch/index.ts
@@ -0,0 +1,4 @@
+export {
+  default as VSearchInput,
+  type Props as VSearchProps,
+} from './VSearchInput.vue'
