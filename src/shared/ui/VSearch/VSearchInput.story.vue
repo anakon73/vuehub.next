dc4408a276fc1992fd1b56a992aff89e7a1e707b
anakon73 <nazarkinah73@gmail.com>
2023-05-24 18:50:57 +0300

    docs(VSearchInput): describe stories

---
+++ src/shared/ui/VSearch/VSearchInput.story.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+import { VSearchInput } from './index.js'
+
+const query = ref('')
+</script>
+
+<template>
+  <Story title="Shared/UI/VSearchInput">
+    <VSearchInput v-model:model-value="query" />
+  </Story>
+</template>
