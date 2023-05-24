855d75189e56633d96d215099fa3f62d62fb3d23
anakon73 <nazarkinah73@gmail.com>
2023-05-24 20:50:25 +0300

    docs(VHeader): describe stories

---
+++ src/shared/ui/VHeader/VHeader.story.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import { VHeader } from '.'
+</script>
+
+<template>
+  <Story title="Shared/UI/VHeader">
+    <Variant title="default">
+      <VHeader
+        username="Username"
+      />
+    </Variant>
+    <Variant title="home page">
+      <VHeader
+        home-page
+        username="Username"
+      />
+    </Variant>
+  </Story>
+</template>
