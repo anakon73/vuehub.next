28145da17600f44a0b41ab84fb6b8e795b1f9429
anakon73 <nazarkinah73@gmail.com>
2023-05-25 17:24:05 +0300

    feat(ui): implement layout component

---
+++ src/shared/ui/VLayout/VLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { VHeader } from '@/shared/ui/VHeader'
+
+export type Props = {
+  withSearch?: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <VHeader :with-search="withSearch" username="username" />
+  <slot />
+</template>
