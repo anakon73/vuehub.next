ce5fb49f2a9aa6de50ccaa2d74c530fc4245896d
anakon73 <nazarkinah73@gmail.com>
2023-05-25 17:24:23 +0300

    docs(VLayout): describe stories

---
+++ src/shared/ui/VLayout/VLayout.story.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { VLayout } from '.'
+</script>
+
+<template>
+  <Story title="Shared/UI/VLayout">
+    <Variant title="default">
+      <VLayout>
+        <div class="mt-10 text-center text-5xl text-red-500">
+          Content
+        </div>
+      </VLayout>
+    </Variant>
+    <Variant title="with search">
+      <VLayout with-search>
+        <div class="mt-10 text-center text-5xl text-red-500">
+          Content
+        </div>
+      </VLayout>
+    </Variant>
+  </Story>
+</template>
