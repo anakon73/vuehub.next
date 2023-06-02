9f99eb99fff212738b2d127c27d0824af77322a4
anakon73 <nazarkinah73@gmail.com>
2023-06-02 19:33:21 +0300

    feat(@tanstack/vue-query): provide

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,7 +1,10 @@
 import type { App } from 'vue'
 
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
 import { router } from './router'
 
 export function applyProviders(app: App) {
+  app.use(VueQueryPlugin)
   app.use(router)
 }
