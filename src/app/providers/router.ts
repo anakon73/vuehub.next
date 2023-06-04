2150e506451ad9cbcdbd1f16f209dc41cf1aa874
anakon73 <nazarkinah73@gmail.com>
2023-06-04 19:43:18 +0300

    feat(ProfilePage): add to router

--- src/app/providers/router.ts
+++ src/app/providers/router.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import { HomePage } from '@/pages/HomePage'
+import { ProfilePage } from '@/pages/ProfilePage'
 
 const routes = [
   { name: 'HomePage', path: '/', component: HomePage },
+  { name: 'ProfilePage', path: '/profile', component: ProfilePage },
 ]
 
 export const router = createRouter({
