c65dcb810ac66b33ca6c00c98bb166763ff7a9bb
anakon73 <nazarkinah73@gmail.com>
2023-06-06 01:13:37 +0300

    docs(VRepositoryCard): describe stories

---
+++ src/entities/repository/VRepositoryCard/VRepositoryCard.story.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRepository } from '@/shared/api/repository'
+
+import { VRepositoryCard } from '.'
+
+const { data: repository, isFetching } = useRepository('anakon73', 'vuehub.next')
+</script>
+
+<template>
+  <Story title="Entities/Repository/VRepositoryCard">
+    <VRepositoryCard
+      v-if="repository !== undefined"
+      :is-fetching="isFetching"
+      :repository="repository"
+    />
+  </Story>
+</template>
