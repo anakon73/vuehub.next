e89fa3f32ee2c5cde1eaaddaf944c25e4b5a89bd
anakon73 <nazarkinah73@gmail.com>
2023-06-07 22:20:07 +0300

    docs(RadioButtons): describe stories

---
+++ src/pages/ProfilePage/ui/RadioButtons/RadioButtons.story.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RadioButtons } from '.'
+
+const selected = ref('repos')
+</script>
+
+<template>
+  <Story title="Pages/ProfilePage/UI/RadioButtons">
+    <RadioButtons
+      v-model="selected"
+    />
+    <template #controls>
+      <div>
+        Starred:
+        <input
+          v-model="selected"
+          value="starred"
+          type="radio"
+        >
+      </div>
+      <div>
+        Repos:
+        <input
+          v-model="selected"
+          value="repos"
+          type="radio"
+        >
+      </div>
+    </template>
+  </Story>
+</template>
