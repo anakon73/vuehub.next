9c47a18788e2d7025a46e076561c9f54178ef367
anakon73 <nazarkinah73@gmail.com>
2023-07-05 14:38:55 +0300

    docs(VSelect): describe stories

---
+++ src/shared/ui/VSelect/VSelect.story.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { type Branch } from '@/shared/types'
+
+import { VSelect } from '.'
+
+const branches = [
+  {
+    name: 'dev',
+    commit: {
+      sha: '74a892b48172563cb3a7cdc10ae346e31706641b',
+      url: 'https://api.github.com/repos/anakon73/vuehub.next/commits/74a892b48172563cb3a7cdc10ae346e31706641b',
+    },
+    protected: false,
+  },
+  {
+    name: 'master',
+    commit: {
+      sha: '74a892b48172563cb3a7cdc10ae34fd31706641b',
+      url: 'https://api.github.com/repos/anakon73/vuehub.next/commits/74a892b48172563cb3a7cdc10ae34fd31706641b',
+    },
+    protected: false,
+  },
+]
+
+const selectedBranch = ref<Branch>(branches[0])
+</script>
+
+<template>
+  <Story title="Shared/UI/VSelect">
+    <VSelect v-model="selectedBranch" :options="branches" />
+  </Story>
+</template>
