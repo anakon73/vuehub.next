e5fb629f84272b32cc23afdc68f964b5a47e4ae5
anakon73 <nazarkinah73@gmail.com>
2023-07-06 15:06:19 +0300

    ci(vite): use plugin for rewrite dot in url

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,7 @@ export default defineConfig({
         },
       ],
     }),
+    pluginRewriteAll(),
   ],
   resolve: {
     alias: {
