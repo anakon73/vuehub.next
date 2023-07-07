06a27f499ca5555a6aa17820839492a2b0e18e1d
anakon73 <nazarkinah73@gmail.com>
2023-07-07 15:06:48 +0300

    feat(router): connect RepositoryPage to router

--- src/app/providers/router.ts
+++ src/app/providers/router.ts
@@ -2,10 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import { HomePage } from '@/pages/HomePage'
 import { ProfilePage } from '@/pages/ProfilePage'
+import { RepositoryPage } from '@/pages/RepositoryPage'
 
 const routes = [
   { name: 'HomePage', path: '/', component: HomePage },
   { name: 'ProfilePage', path: '/profile', component: ProfilePage },
+  {
+    name: 'RepositoryPage',
+    path: '/:user/:repo',
+    component: RepositoryPage,
+    props: true,
+  },
 ]
 
 export const router = createRouter({
