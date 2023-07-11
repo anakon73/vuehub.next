2294ca9e3ed1f68d933c6a75d2caa082056a38fb
anakon73 <nazarkinah73@gmail.com>
2023-07-11 18:39:30 +0300

    docs(RepositoryPage): add debounce for user and repo props

--- src/pages/RepositoryPage/RepositoryPage.story.vue
+++ src/pages/RepositoryPage/RepositoryPage.story.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 import { RepositoryPage } from '.'
+
+const user = ref('anakon73')
+const repo = ref('carvago-next')
+
+const userDebounced = refDebounced(user, 2000)
+const repoDebounced = refDebounced(repo, 2000)
 </script>
 
 <template>
   <Story title="Pages/RepositoryPage">
-    <RepositoryPage
-      user="anakon73"
-      repo="vuehub.next"
-    />
+    <Variant title="default">
+      <RepositoryPage
+        :user="userDebounced"
+        :repo="repoDebounced"
+      />
+      <template #controls>
+        <HstText v-model="user" />
+        <HstText v-model="repo" />
+      </template>
+    </Variant>
   </Story>
 </template>
