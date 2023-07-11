829e2da1cd4c92d694aee8ad5e7f176c9c9a60fd
anakon73 <nazarkinah73@gmail.com>
2023-07-11 18:36:34 +0300

    docs(ProfilePage): add debounce for user prop

--- src/pages/ProfilePage/ProfilePage.story.vue
+++ src/pages/ProfilePage/ProfilePage.story.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import { ProfilePage } from '.'
+
+const user = ref('extrem7')
+const userDebounced = refDebounced(user, 2000)
 </script>
 
 <template>
   <Story title="Pages/ProfilePage">
-    <ProfilePage user="extrem7" />
+    <Variant title="default">
+      <ProfilePage :user="userDebounced" />
+      <template #controls>
+        <HstText v-model="user" />
+      </template>
+    </Variant>
   </Story>
 </template>
