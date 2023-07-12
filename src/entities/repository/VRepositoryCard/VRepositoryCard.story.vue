6efd93b1704e07c0223eff1d247f8612e6ce65ac
anakon73 <nazarkinah73@gmail.com>
2023-07-12 15:01:28 +0300

    docs(VRepositoryCard): add debouced user and repo props

--- src/entities/repository/VRepositoryCard/VRepositoryCard.story.vue
+++ src/entities/repository/VRepositoryCard/VRepositoryCard.story.vue
@@ -3,15 +3,30 @@ import { useRepository } from '@/shared/api/repository'
 
 import { VRepositoryCard } from '.'
 
-const { data: repository, isFetching } = useRepository('anakon73', 'vuehub.next')
+const user = ref('anakon73')
+const repo = ref('vuehub.next')
+
+const userDebounced = refDebounced(user, 2000)
+const repoDebounced = refDebounced(repo, 2000)
+
+const {
+  data: repository,
+  isFetching,
+} = useRepository(userDebounced, repoDebounced)
 </script>
 
 <template>
   <Story title="Entities/Repository/VRepositoryCard">
-    <VRepositoryCard
-      v-if="repository !== undefined"
-      :is-fetching="isFetching"
-      :repository="repository"
-    />
+    <Variant title="default">
+      <VRepositoryCard
+        v-if="repository !== undefined"
+        :is-fetching="isFetching"
+        :repository="repository"
+      />
+      <template #controls>
+        <HstText v-model="user" />
+        <HstText v-model="repo" />
+      </template>
+    </Variant>
   </Story>
 </template>
