16a3ebb52cdf7b7bb6f92bc93a04e151c089816c
anakon73 <nazarkinah73@gmail.com>
2023-07-20 14:02:47 +0300

    docs(SearchForm): describe stories

---
+++ src/widgets/SearchForm/SearchForm.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { SearchForm } from '.'
+</script>
+
+<template>
+  <Story title="Widgets/SearchForm">
+    <Variant title="default">
+      <SearchForm />
+    </Variant>
+  </Story>
+</template>
