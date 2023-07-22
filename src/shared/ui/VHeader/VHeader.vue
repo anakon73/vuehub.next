8bc0b0a007bc0d7273ae221d129866f5b5b0cbdc
anakon73 <nazarkinah73@gmail.com>
2023-07-22 23:31:11 +0300

    refactor(VHeader): use max width instead padding

--- src/shared/ui/VHeader/VHeader.vue
+++ src/shared/ui/VHeader/VHeader.vue
@@ -11,8 +11,8 @@ defineProps<Props>()
 </script>
 
 <template>
-  <header class="fixed top-0 z-20 w-full bg-neutral-800">
-    <div class="flex justify-between px-[13.75rem] py-4">
+  <header class="fixed top-0 z-20 flex w-full justify-center bg-neutral-800">
+    <div class="flex w-full max-w-[1150px] justify-between py-4">
       <div class="flex items-center gap-5">
         <div class="h-12 w-12 rounded-full bg-white" />
         <SearchForm />
