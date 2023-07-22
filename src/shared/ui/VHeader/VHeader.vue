e80a9a4055ad60993f4686463a6ea733e40c88e2
anakon73 <nazarkinah73@gmail.com>
2023-07-22 23:35:06 +0300

    feat(VHeader): route to home page

--- src/shared/ui/VHeader/VHeader.vue
+++ src/shared/ui/VHeader/VHeader.vue
@@ -14,7 +14,9 @@ defineProps<Props>()
   <header class="fixed top-0 z-20 flex w-full justify-center bg-neutral-800">
     <div class="flex w-full max-w-[1150px] justify-between py-4">
       <div class="flex items-center gap-5">
-        <div class="h-12 w-12 rounded-full bg-white" />
+        <RouterLink to="/">
+          <div class="h-12 w-12 rounded-full bg-white" />
+        </RouterLink>
         <SearchForm />
       </div>
       <VButton style-variant="primary" class="px-5 py-3.5">
