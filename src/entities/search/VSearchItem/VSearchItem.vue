69f1eba5bae89df214b9014ef661468c0a2247e0
anakon73 <nazarkinah73@gmail.com>
2023-07-22 23:44:17 +0300

    feat(VSearchItem): implement dynamic route to user & repository pages

--- src/entities/search/VSearchItem/VSearchItem.vue
+++ src/entities/search/VSearchItem/VSearchItem.vue
@@ -13,24 +13,36 @@ const { user, repository } = toRefs(props)
 </script>
 
 <template>
-  <div
-    class="
-    flex items-center gap-2 whitespace-nowrap
-    bg-neutral-800 py-1 font-medium text-zinc-100
-    "
-  >
-    <BookmarkSquareIcon class="h-4 w-4" />
-    <div
-      v-if="user"
-      class="max-w-[11.25rem] truncate"
-    >
-      {{ user.login }}
-    </div>
-    <div
-      v-if="repository"
-      class="max-w-[11.25rem] truncate"
-    >
-      {{ repository.fullName }}
-    </div>
+  <div>
+    <RouterLink v-if="user" :to="{ path: `/${user.login}` }">
+      <div
+        class="
+        flex items-center gap-2 whitespace-nowrap
+        bg-neutral-800 py-1 font-medium text-zinc-100
+        "
+      >
+        <BookmarkSquareIcon class="h-4 w-4" />
+        <div
+          class="max-w-[11.25rem] truncate"
+        >
+          {{ user.login }}
+        </div>
+      </div>
+    </RouterLink>
+    <RouterLink v-if="repository" :to="{ path: `/${repository.fullName}` }">
+      <div
+        class="
+        flex items-center gap-2 whitespace-nowrap
+        bg-neutral-800 py-1 font-medium text-zinc-100
+        "
+      >
+        <BookmarkSquareIcon class="h-4 w-4" />
+        <div
+          class="max-w-[11.25rem] truncate"
+        >
+          {{ repository.fullName }}
+        </div>
+      </div>
+    </RouterLink>
   </div>
 </template>
