87f95fab03eded8a5553f946c3dab9950f37b6b0
anakon73 <nazarkinah73@gmail.com>
2023-07-25 18:36:49 +0300

    feat(SearchForm): implement vueuse functions for close results list & blur input

--- src/widgets/SearchForm/SearchForm.vue
+++ src/widgets/SearchForm/SearchForm.vue
@@ -7,6 +7,10 @@ import { VInput } from '@/shared/ui/VInput'
 const query = ref('')
 const queryDebounced = refDebounced(query, 1000)
 
+const form = ref()
+
+const isShow = ref(false)
+
 const { data: users } = useSearchUsers(queryDebounced)
 const { data: repositories } = useSearchRepositories(queryDebounced)
 
@@ -18,17 +22,27 @@ const usersArray = computed(() => {
   if (users.value !== undefined)
     return users.value.items.splice(0, 3)
 })
+
+onClickOutside(form, () => isShow.value = false)
+
+onKeyStroke('Escape',
+  () => {
+    (document.activeElement as HTMLElement).blur()
+    isShow.value = false
+  },
+)
 </script>
 
 <template>
-  <div class="relative">
+  <div ref="form" class="relative">
     <VInput
       v-model:model-value="query"
       style-variant="white"
       placeholder="Search..."
+      @click="isShow = true"
     />
     <div
-      v-if="queryDebounced"
+      v-if="queryDebounced && isShow"
       class="
       absolute top-14 w-[13.4375rem] rounded-b-xl bg-neutral-800
       px-4 py-2 shadow-md
