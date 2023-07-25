f334ca91bb2c6c4c2b5f9d875ce650c15e6feae7
anakon73 <nazarkinah73@gmail.com>
2023-07-25 16:25:53 +0300

    docs(RegisterPage): describe stories

---
+++ src/pages/RegisterPage/RegisterPage.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RegisterPage } from '.'
+</script>
+
+<template>
+  <Story title="Pages/RegisterPage">
+    <Variant title="default">
+      <RegisterPage />
+    </Variant>
+  </Story>
+</template>
