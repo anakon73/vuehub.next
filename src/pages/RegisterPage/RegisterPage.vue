72ba995f2dd419de675753646346516e7bcd3bf7
anakon73 <nazarkinah73@gmail.com>
2023-07-25 16:25:35 +0300

    feat(pages): implement register page

---
+++ src/pages/RegisterPage/RegisterPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RegisterForm } from '@/widgets/RegisterForm'
+
+import { VLayout } from '@/shared/ui/VLayout'
+
+import google from './assets/googleIcon.svg'
+import facebook from './assets/facebookIcon.svg'
+</script>
+
+<template>
+  <VLayout>
+    <div
+      class="mx-auto max-w-[30.625rem] pt-40 font-roboto"
+    >
+      <RegisterForm />
+      <div class="pt-14 text-center text-2xl">
+        or sign in with
+      </div>
+      <div class="flex justify-center gap-5 pt-3">
+        <img :src="google">
+        <img :src="facebook">
+      </div>
+    </div>
+  </VLayout>
+</template>
---
+++ src/pages/RegisterPage/index.ts
@@ -0,0 +1,3 @@
+export {
+  default as RegisterPage,
+} from './RegisterPage.vue'
