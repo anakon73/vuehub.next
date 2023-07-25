2ef837208845fe50d4cac4f1d46b526b3eaedc27
anakon73 <nazarkinah73@gmail.com>
2023-07-25 14:13:30 +0300

    docs(RegisterForm): describe stories

---
+++ src/widgets/RegisterForm/RegisterForm.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RegisterForm } from '.'
+</script>
+
+<template>
+  <Story title="Widgets/RegisterForm">
+    <Variant title="default">
+      <RegisterForm />
+    </Variant>
+  </Story>
+</template>
