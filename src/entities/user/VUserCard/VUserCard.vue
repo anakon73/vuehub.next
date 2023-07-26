f104749edca38c5377201896d2a6d96d3e51a679
anakon73 <nazarkinah73@gmail.com>
2023-07-26 21:57:44 +0300

    feat(entities): implement user card

---
+++ src/entities/user/VUserCard/VUserCard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { SearchUserItem } from '@/shared/types'
+import { VButton } from '@/shared/ui/VButton'
+
+type Props = {
+  user: SearchUserItem
+}
+
+const props = defineProps<Props>()
+
+const { user } = toRefs(props)
+</script>
+
+<template>
+  <div
+    :="$attrs"
+    class="
+    flex items-start justify-between
+    rounded-xl bg-zinc-100 p-4 font-robotomono text-neutral-800
+    "
+  >
+    <div class="flex gap-4">
+      <div>
+        <img
+          class="h-24 w-24 rounded-full"
+          :src="user.avatarUrl"
+          alt="user avatar"
+        >
+      </div>
+      <div>
+        <div
+          class="text-2xl font-medium"
+        >
+          {{ user.login }}
+        </div>
+      </div>
+    </div>
+    <VButton
+      style-variant="regular"
+      class="border border-black/40 px-8 py-2 text-lg font-normal"
+    >
+      follow
+    </VButton>
+  </div>
+</template>
---
+++ src/entities/user/VUserCard/index.ts
@@ -0,0 +1,3 @@
+export {
+  default as VUserCard,
+} from './VUserCard.vue'
