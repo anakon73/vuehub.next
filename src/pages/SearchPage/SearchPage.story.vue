a76cd4d3e503bc0eca7a9abbb03aa584c1a46f88
anakon73 <nazarkinah73@gmail.com>
2023-07-28 17:57:20 +0300

    docs(SearchPage): describe stories

---
+++ src/pages/SearchPage/SearchPage.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { SearchPage } from '.'
+</script>
+
+<template>
+  <Story title="Pages/SearchPage">
+    <Variant title="default">
+      <SearchPage />
+    </Variant>
+  </Story>
+</template>
