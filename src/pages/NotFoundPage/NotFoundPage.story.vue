363e72907f4935dcdbac972ada7fffa560eb4aa5
anakon73 <nazarkinah73@gmail.com>
2023-07-28 19:59:18 +0300

    docs(NotFoundPage): describe stories

---
+++ src/pages/NotFoundPage/NotFoundPage.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { NotFoundPage } from '.'
+</script>
+
+<template>
+  <Story title="Pages/NotFoundPage">
+    <Variant title="default">
+      <NotFoundPage />
+    </Variant>
+  </Story>
+</template>
