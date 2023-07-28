648e376b78fea1b5fe732bee0bf928381eae342c
anakon73 <nazarkinah73@gmail.com>
2023-07-28 17:49:54 +0300

    feat(App): use VLayout in root component

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { VLayout } from '@/shared/ui/VLayout'
 </script>
 
 <template>
-  <RouterView />
+  <VLayout>
+    <RouterView />
+  </VLayout>
 </template>
