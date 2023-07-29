eae99e47f0cccf8976366c6377eef97e8992ca2e
anakon73 <nazarkinah73@gmail.com>
2023-07-29 17:28:46 +0300

    docs(VPagination): describe stories

---
+++ src/shared/ui/VPagination/VPagination.story.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { VPagination } from '.'
+
+const page = ref(1)
+const totalPages = 4
+</script>
+
+<template>
+  <Story title="Shared/UI/VPagination">
+    <Variant title="default">
+      <VPagination
+        :total-pages="totalPages"
+        :current-page="page"
+        @prev-page="page -= 1"
+        @first-page="page = 1"
+        @next-page="page += 1"
+        @last-page="page = totalPages"
+      />
+    </Variant>
+  </Story>
+</template>
