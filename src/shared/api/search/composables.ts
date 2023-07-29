f06183414bff878073e28f52092040543fe86bec
anakon73 <nazarkinah73@gmail.com>
2023-07-29 17:29:08 +0300

    feat(API): implement pagination

--- src/shared/api/search/composables.ts
+++ src/shared/api/search/composables.ts
@@ -2,18 +2,24 @@ import { useQuery } from '@tanstack/vue-query'
 
 import { searchKeys, searchRepositoriesFetcher, searchUsersFetcher } from '.'
 
-export function useSearchUsers(query: Ref<string>) {
-  return useQuery(searchKeys.GetUsers(query),
-    () => searchUsersFetcher(unref(query)), {
+export function useSearchUsers(
+  query: Ref<string>,
+  page: Ref<number> = ref(1),
+) {
+  return useQuery(searchKeys.GetUsers(query, page),
+    () => searchUsersFetcher(unref(query), unref(page)), {
       keepPreviousData: false,
       refetchOnWindowFocus: false,
       enabled: computed(() => !!query.value.length),
     })
 }
 
-export function useSearchRepositories(query: Ref<string>) {
-  return useQuery(searchKeys.GetRepositories(query),
-    () => searchRepositoriesFetcher(unref(query)), {
+export function useSearchRepositories(
+  query: Ref<string>,
+  page: Ref<number> = ref(1),
+) {
+  return useQuery(searchKeys.GetRepositories(query, page),
+    () => searchRepositoriesFetcher(unref(query), unref(page)), {
       keepPreviousData: false,
       refetchOnWindowFocus: false,
       enabled: computed(() => !!query.value.length),
--- src/shared/api/search/query.ts
+++ src/shared/api/search/query.ts
@@ -8,29 +8,34 @@ import {
 } from '.'
 
 export const searchKeys = {
-  GetUsers: (query: Ref<string>) => ['users', query],
-  GetRepositories: (query: Ref<string>) => ['repositories', query],
+  GetUsers: (query: Ref<string>, page: Ref<number>) => ['users', query, page],
+  GetRepositories: (query: Ref<string>, page: Ref<number>) =>
+    ['repositories', query, page],
 }
 
 export const searchEndpoints = {
-  getUsers: (query: string) => `/search/users?q=${query}`,
-  getRepositories: (query: string) => `/search/repositories?q=${query}`,
+  getUsers: (query: string, page: number) => {
+    return `/search/users?q=${query}&per_page=10&page=${page}`
+  },
+  getRepositories: (query: string, page: number) => {
+    return `/search/repositories?q=${query}&per_page=10&page=${page}`
+  },
 }
 
-export async function searchUsersFetcher(query: string) {
+export async function searchUsersFetcher(query: string, page: number) {
   return normalizeSearchUsers(
     searchUserAPI
       .parse(await fetch(
-      `${BASE_URL}${searchEndpoints.getUsers(query)}`,
+      `${BASE_URL}${searchEndpoints.getUsers(query, page)}`,
       ).then((r) => r.json())),
   )
 }
 
-export async function searchRepositoriesFetcher(query: string) {
+export async function searchRepositoriesFetcher(query: string, page: number) {
   return normalizeSearchRepositories(
     searchRepositoryAPI
       .parse(await fetch(
-      `${BASE_URL}${searchEndpoints.getRepositories(query)}`,
+      `${BASE_URL}${searchEndpoints.getRepositories(query, page)}`,
       ).then((r) => r.json())),
   )
 }
